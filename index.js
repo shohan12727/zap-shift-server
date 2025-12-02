b47ecf37e1af65e79a1554a6c65266ceb491c973
shohan12727 <aishohan001@gmail.com>
2025-12-02 18:08:35 +0600

    delete api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -46,8 +46,13 @@ async function run() {
       const parcels = req.body;
       // parcel created time
       parcels.createdAt = new Date();
-
       const result = await parcelsCollection.insertOne(parcels);
+      res.send(result);
+    });
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelsCollection.deleteOne(query);
       res.send(result);
     });
 
